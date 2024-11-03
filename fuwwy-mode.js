6595b245b5aa7073d20e7adcd7fb2ffa2d1e58e7
BenSav <153027450+BenSav8@users.noreply.github.com>
2024-11-02 22:29:20 -0300

    added

---
+++ fuwwy-mode.js
@@ -0,0 +1,7 @@
+addons.register('messageReciever', function(data) {
+	data.msg = data.msg.toLowerCase().replaceAll('r', 'w').replaceAll('l', 'w').replaceAll(' g', ' g-g').replaceAll(' b', ' b-b').replaceAll('qu', 'qw') + " /kao"
+})
+
+addons.register('messageSender', function(data) {
+	data.msg = data.msg.toLowerCase().replaceAll('r', 'w').replaceAll('l', 'w').replaceAll(' g', ' g-g-g').replaceAll(' b', ' b-b').replaceAll('qu', 'qw') + " /kao"
+})
