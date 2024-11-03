2d3abcbfea5946a6f76f25e44a85c950760dda8e
BenSav <153027450+BenSav8@users.noreply.github.com>
2024-11-02 22:29:54 -0300

    removed sender

--- fuwwy-mode.js
+++ fuwwy-mode.js
@@ -1,7 +1,3 @@
 addons.register('messageReciever', function(data) {
 	data.msg = data.msg.toLowerCase().replaceAll('r', 'w').replaceAll('l', 'w').replaceAll(' g', ' g-g').replaceAll(' b', ' b-b').replaceAll('qu', 'qw') + " /kao"
 })
-
-addons.register('messageSender', function(data) {
-	data.msg = data.msg.toLowerCase().replaceAll('r', 'w').replaceAll('l', 'w').replaceAll(' g', ' g-g-g').replaceAll(' b', ' b-b').replaceAll('qu', 'qw') + " /kao"
-})
