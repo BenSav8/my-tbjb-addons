fbc560fc47d10ecf48ac3f03a6f7dfae1fa157d3
BenSav <153027450+BenSav8@users.noreply.github.com>
2025-01-11 15:43:18 -0300

    added @ here, highlights pings, ...
    ...improved colors, pings directed to others now have color, fixed /everyone not saving

--- ping.js
+++ ping.js
@@ -1,12 +1,17 @@
 let hasPinged = false;
 let ping = new Audio($store.get('.config/trollbox/pingsound') || 'https://file.garden/ZeybJgZmvAenbkoN/discord_ping_sound_effect.mp3');
-let everyone = $store.get('.config/trollbox/pingeveryone') || true;
+let everyone = $store.get('.config/trollbox/pingeveryone') || "true";
 let focusInterval;
+let pingusers = {}
+
+socket.on('update users', function(data) {
+	pingusers = data
+});
 
 addons.register('messageReciever', function(data) {
-    if (data.msg.includes('@' + pseudo)) {
+    if (data.msg.includes('@everyone') && everyone == "true") {
         hasPinged = true;
-        data.msg = data.msg.replaceAll('@' + pseudo, '@$(' + color + ')' + pseudo + '$(white)');
+        data.msg = '<span style="background-color: #ffff0020;">' + data.msg.replaceAll('@everyone', '@<span style="color: #c3ff00;">everyone</span>') + '</span>';
         if (!document.hasFocus()) {
             if (!focusInterval) {
                 focusInterval = setInterval(function () {
@@ -26,12 +31,20 @@ addons.register('messageReciever', function(data) {
             clearInterval(focusInterval);
             focusInterval = null;
         }
-        
         ping.play();
     }
-    if (data.msg.includes('@everyone') && everyone) {
+    if (data.msg.includes('@here')) {
         hasPinged = true;
-        data.msg = data.msg.replaceAll('@everyone', '@$(#c3ff00)everyone$(white)');
+		if (document.hasFocus()) {
+			data.msg = '<span style="background-color: #ffff0020;">' + data.msg.replaceAll('@here', '@<span style="color: #c3ff00;">here</span>') + '</span>';
+        } else {
+			data.msg = data.msg.replaceAll('@here', '@<span style="color: #c3ff00;">here</span>');
+		}
+		ping.play();
+    }
+    if (he.decode(data.msg).includes('@' + pseudo)) {
+        hasPinged = true;
+        data.msg = '<span style="background-color: #ffff0020;">' + he.decode(data.msg).replaceAll('@' + pseudo, '@<span style="color: ' + color + ';">' + pseudo + '</span>') + '</span>';
         if (!document.hasFocus()) {
             if (!focusInterval) {
                 focusInterval = setInterval(function () {
@@ -51,15 +64,22 @@ addons.register('messageReciever', function(data) {
             clearInterval(focusInterval);
             focusInterval = null;
         }
-        
         ping.play();
     }
+	for (const key in pingusers) {
+		if (pingusers.hasOwnProperty(key)) {
+			if (pingusers[key].nick != pseudo && he.decode(data.msg).includes('@' + he.decode(pingusers[key].nick))) {
+				data.msg = he.decode(data.msg).replaceAll('@' + he.decode(pingusers[key].nick), '@<span style="color: ' + pingusers[key].color + ';">' + he.decode(pingusers[key].nick) + '</span>')
+			}
+		}
+	}
+
 });
 
 addons.register('messageSender', function(data) {
     if (data.msg == "/ping") {
         data.msg = '';
-        let url = prompt('Please input a sound URL. (blank for default (discord), i recommend filegarden)');
+        let url = prompt('Please input a sound URL. (blank for default (discord), i recommend filegarden or catbox)');
         if (url == '') {
             url = "https://file.garden/ZeybJgZmvAenbkoN/discord_ping_sound_effect.mp3";
         }
@@ -71,13 +91,13 @@ addons.register('messageSender', function(data) {
         data.msg = '';
         if (msg == "/everyone on") {
             printMsg({ date: Date.now(), nick: "~", color: "white", style: "opacity: 0.7;", home: 'local', msg: "@every\u2062one turned on." });
-            $store.set('.config/trollbox/pingeveryone', true);
-            everyone = true;
+            $store.set('.config/trollbox/pingeveryone', "true");
+            everyone = "true";
         }
         if (msg == "/everyone off") {
             printMsg({ date: Date.now(), nick: "~", color: "white", style: "opacity: 0.7;", home: 'local', msg: "@every\u2062one turned off." });
-            $store.set('.config/trollbox/pingeveryone', false);
-            everyone = false;
+            $store.set('.config/trollbox/pingeveryone', "false");
+            everyone = "false";
         }
     }
 });
