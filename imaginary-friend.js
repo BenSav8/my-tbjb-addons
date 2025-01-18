dd7bdfb244bd4e8ecd069b5e77b07984ba0311f6
BenSav <153027450+BenSav8@users.noreply.github.com>
2025-01-18 00:59:23 -0300

    add /despawn

--- imaginary-friend.js
+++ imaginary-friend.js
@@ -18,28 +18,44 @@ addons.register('messageSender', async function(data) {
         const nameword = await getword();
         const itsname = capitalize(nameword) + Math.floor(Math.random() * 9999);
         const itscolor = Math.floor(Math.random() * 360);
-        
+		let intb = true
         printMsg({
             date: Date.now(),
             color: '#0f0',
             nick: '→',
-            home: '███████',
+            home: 'local',
             msg: '<span class="trollbox_nick" style="color: hsl(' + itscolor + ', 45%, 45%)">' + itsname + '</span> <em>has entered teh trollbox?</em>'
         });
-        setInterval(async () => {
+		const friend = setInterval(async () => {
             const word1 = await getword();
             const word2 = await getword();
 			const word3 = await getword();
-            printMsg({
-                date: Date.now(),
-                color: 'hsl(' + itscolor + ', 45%, 45%)',
-                nick: itsname,
-                home: '███████',
-                msg: capitalize(word1) + ' ' + word2 + ' ' + word3 + '!'
-            });
+			if (intb) {
+				printMsg({
+					date: Date.now(),
+					color: 'hsl(' + itscolor + ', 45%, 45%)',
+					nick: itsname,
+					home: '███████',
+					msg: capitalize(word1) + ' ' + word2 + ' ' + word3 + '!'
+				});
+			}
 			if (noFocusMsg >= 0) {
 				noFocusMsg = noFocusMsg + 1
 			}
         }, 10000);
+		addons.register('messageSender', async function(data) {
+			if (data.msg == "/despawn") {
+				data.msg == ''
+				clearInterval(friend)
+				intb = false
+				printMsg({
+					date: Date.now(),
+					color: '#0f0',
+					nick: '→',
+					home: 'local',
+					msg: '<span class="trollbox_nick" style="color: hsl(' + itscolor + ', 45%, 45%)">' + itsname + '</span> <em>has left teh trollbox.</em>'
+				});
+			}
+		});
     }
 });
